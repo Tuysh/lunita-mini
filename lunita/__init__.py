a9335015b7f92b1a2bf586851ba81dd2be6a0cb8
Tuysh <angelmacloviomorales@gmail.com>
2026-01-10 20:00:43 -0600

    feat: añadir ErroresMagicos a la exportación del módulo

--- lunita/__init__.py
+++ lunita/__init__.py
@@ -3,5 +3,12 @@ from groq.types.chat import ChatCompletionMessageParam as Mensaje
 from .configuracion import ConfigurarEstrellas
 from .sesion import Sesion
 from .sesion_asincrona import SesionAsincrona
+from .utils.errores import ErroresMagicos
 
-__all__ = ["Sesion", "SesionAsincrona", "ConfigurarEstrellas", "Mensaje"]
+__all__ = [
+    "Sesion",
+    "SesionAsincrona",
+    "ConfigurarEstrellas",
+    "Mensaje",
+    "ErroresMagicos",
+]
